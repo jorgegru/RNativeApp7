af5483af89d0dd77a6b47409a4df76c32f085715
Jorge Goulart <jorgegru@gmail.com>
2020-03-23 14:53:26 -0300

    174. Botão calcular e o contexto léxico

--- src/components/Painel.js
+++ src/components/Painel.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text} from 'react-native';
 import Entrada from './Entrada';
 import Operacao from './Operacao';
 import Comando from './Comando';
 
-const Painel = props => (
-    <View>
-        <Entrada />
-        <Operacao />
-        <Comando />
-    </View>
-);
+
+class Painel extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { num1: '1', num2: '2'}
+        this.calcular = this.calcular.bind(this);
+    }
+
+    calcular() {
+        const resultado = parseFloat(this.state.num1) +parseFloat(this.state.num2);
+        
+        console.log(resultado);
+    }
+
+    render() {
+        return (
+            <View>
+                <Entrada num1={ this.state.num1 }  num2={ this.state.num2 }/>
+                <Operacao />
+                <Comando acao={ this.calcular } />
+            </View>
+        );
+    }
+}
+
 
 export { Painel };
